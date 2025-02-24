a97c30dafaff8625ae07dcadd5c74e1dd27d7d59
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-02-25 00:13:00 +0530

    [Adarsh] Add. EmployeePayrollData model to store employee details

---
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.employeepayrollapp.model;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+
+public class EmployeePayrollData {
+    private int employeeId;
+    private String name;
+    private long salary;
+
+    public EmployeePayrollData() {}
+
+    public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) {
+        this.employeeId = employeeId;
+        this.name = employeePayrollDTO.name;
+        this.salary = employeePayrollDTO.salary;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+}
