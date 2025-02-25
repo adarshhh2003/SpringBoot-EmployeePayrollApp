2ec006c3f01752b1ee86b6d4c4baee5432a93b4d
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-02-25 11:50:12 +0530

    [Adarsh] Add. EmployeePayrollService with CRUD methods

---
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.employeepayrollapp.services;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeePayrollService implements IEmployeePayrollService{
+
+    public List<EmployeePayrollData> getEmployeePayrollData() {
+        List<EmployeePayrollData> empDataList = new ArrayList<>();
+        empDataList.add(new EmployeePayrollData(1, new EmployeePayrollDTO("Uday", 30000)));
+        return empDataList;
+    }
+
+    public EmployeePayrollData getEmployeePayrollDataById(int empId) {
+        EmployeePayrollData empData = null;
+        empData = new EmployeePayrollData(empId, new EmployeePayrollDTO("Uday", 30000));
+        return empData;
+    }
+
+    public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+        EmployeePayrollData empData = null;
+        empData = new EmployeePayrollData(1, empPayrollDTO);
+        return empData;
+    }
+
+    public EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
+        EmployeePayrollData empData = null;
+        empData = new EmployeePayrollData(1, employeePayrollDTO);
+        return empData;
+    }
+
+    public void deleteEmployeePayrollData(int empId) {
+
+    }
+}
