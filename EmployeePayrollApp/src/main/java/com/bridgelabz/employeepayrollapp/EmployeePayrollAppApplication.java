fb6f172431d78a0d3e939fb2b0b60dae88c0834b
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-02-25 16:56:24 +0530

    [Adarsh] Add. log the data source username

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -13,7 +13,7 @@ public class EmployeePayrollAppApplication {
 		ApplicationContext context = SpringApplication
 										.run(EmployeePayrollAppApplication.class, args);
 		log.info("Employee Payroll App Started in {} Environment",
-				context.getEnvironment().getProperty("environment"));
+				context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
