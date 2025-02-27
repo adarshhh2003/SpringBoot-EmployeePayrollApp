d179a8dff5ee8e715edcb66c622bdc80c5310544
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-02-27 12:21:37 +0530

    [Adarsh] Add. validation pattern for name in EmployeePayrollDTO

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,6 +1,12 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+
+    @NotEmpty(message = "Name cannot be empty")
+    @Pattern(regexp = "^[A-Z][a-zA-Z]{2,}+\\s[A-Z][a-zA-Z]{2,}$", message = "The first and last name should start with capital letters separated by space")
     public String name;
     public long salary;
 
