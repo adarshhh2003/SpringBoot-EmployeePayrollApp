ab5d307860ab34fa3740f92480c18834a4356918
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-02-28 16:22:48 +0530

    [Adarsh] Add. handled the exception for invalid date format

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/exceptions/GlobalExceptionHandler.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.bridgelabz.employeepayrollapp.exceptions;
 
 import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,7 +14,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+
+    private static final String message = "Exception while processing REST Request";
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDTO> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        log.error("Invaid Date Format", ex);
+        ResponseDTO responseDTO=new ResponseDTO(message, "Should have date in the format dd MMM yyyy");
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseDTO> handleValidationExceptions(MethodArgumentNotValidException ex) {
